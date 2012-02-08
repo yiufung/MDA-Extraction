ad06831d2aa342a8e5a1d9e48a84dcb1efccc901
Yiufung Cheong <yzhangak@ust.hk>
2012-02-08 20:50:28 +0800

    With some modification extract.pl but dont work on 4

--- test2/extract.pl
+++ test2/extract.pl
@@ -1,27 +1,38 @@
 #!/usr/bin/local/perl
+use utf8;
+
+# Usage:
+# perl extract.pl <filename>
+# The script will then generate <filename>_7 and <filename>_7a if the corresponding item is extracted.
+# Whether it successfully extracts or not, it will generate <filename>_out, which contains the plain text after decoding from HTML.
+# You can feed <filename>_out to extract_MDA.pl to check the difference.
+# You can use rm *_7 *_7a *_out to remove generated files.
 
 package HTMLStrip;
 use base "HTML::Parser";
 use HTML::Entities;
 
 $filename=$ARGV[0];
-print "opening $filename.\n";
+open (FILE, "< $filename")
+  or die "Can't open $filename: $!\n";
+print "reading from $filename.\n";
 
 $content = "";
 
-#All regular expressions use 2 modifiers, i, x, s, and g
-#i for case insensitivity, so "ITEM" is the same as "item".
-#x for adding whitespace inside regular expresssions, so we can add comments to improve readability.
-#s for treating the expression as string. so . match literally anything, even \n
-#g for global matching.
-#Note: ? added after + or * (such as .*? \s+?) are for non-greedy pattern matching.
+# All regular expressions use 2 modifiers, i, x, s, and g
+# i for case insensitivity, so "ITEM" is the same as "item".
+# x for adding whitespace inside regular expresssions, so we can add comments to improve readability.
+# s for treating the expression as string. so . match literally anything, even \n
+# g for global matching.
+# Note: ? added after + or * (such as .*? \s+?) are for non-greedy pattern matching.
 sub Item7{
   $content =~ s/
-    item\s+?7  #item and arbitrary number of spaces.
-    .*?        #anything in between
-    management.s?\s+?discussions?\s+?and\s+?analysis\s+?of\s+?       #the necessary part
-      (financial\s+?conditions?|results\s+?of\s+?operations?)       #"financial conditions" and "results of operations" may come in different order.
-      (\s+?and\s+?results\s+?of\s+?operations?|\s+?and\s+?financial\s+?conditions?)?       #the latter part may not be necessary, so ? mark is used here.
+    (?:[^\"])\s+ # means (not "), so it's not quoting. ?: means just match, don't include it so it won't be subsituted. 
+    ?item\s+?7  # item and arbitrary number of spaces.
+    [^Aa]*?        # anything in between except "Aa", which means "7A" instead.
+    management.s?\s+?discussions?\s+?and\s+?analysis\s+?of\s+?       # the necessary part
+      (financial\s+?conditions?|results\s+?of\s+?operations?)       # "financial conditions" and "results of operations" may come in different order.
+      (\s+?and\s+?results\s+?of\s+?operations?|\s+?and\s+?financial\s+?conditions?)?       # the latter part may not be necessary, so ? mark is used here.
     /
     ######ITEM 7######
     /gixs;
@@ -38,7 +49,7 @@ sub Item7a{
 }
 
 sub Item89{
-  #return true if it's item 8.
+  # return true if it's item 8.
   $content =~ s/
     item\s+?8
     .*?
@@ -46,7 +57,7 @@ sub Item89{
     /
     ######ITEM 8######
     /gixs;
-  #while there isn't item 8.
+  # while there isn't item 8.
   $content =~ s/
     item\s+?9a?
     .*
@@ -60,56 +71,68 @@ sub Item89{
 ## Start ##
 ###########
 
-#global string to store all the content;
+# global string to store all the content;
 $contentInOneLine="";
-#override the method in HTML::Parser to add the target text into $string.
+# override the method in HTML::Parser to add the target text into $string.
 sub text{
   my ($self, $text) = @_;
   $contentInOneLine.=$text;
 }
 
-
 my $p = new HTMLStrip;
-#use <> so we can read from command line.
-while(<>){
+# use <> so we can read from command line.
+while(<FILE>){
   $p->parse($_);
 }
-$p->eof;#flush the parser.
+$p->eof;# flush the parser.
 
-#By so far we get the whole text in one line.
-#Before extracting, substitute all HTML entity code into ASCII character.
-#Visit http://www.w3schools.com/tags/ref_ascii.asp for the whole table.
-#Here we use the HTML::Entities module.
+# By so far we get the whole text in one line.
+# Before extracting, substitute all HTML entity code into ASCII character.
+# Visit http://www.w3schools.com/tags/ref_ascii.asp for the whole table.
+# Here we use the HTML::Entities module.
 $plainTextInOneLine = HTML::Entities::decode($contentInOneLine);
-#start from the 5% so skip the table of contents part.
+# First substitue all UTF-8 character into ascii code. May need to add more due to Perl's inadequate utf-8 support.
+$plainTextInOneLine =~ s//'/gs;
+$plainTextInOneLine =~ s//"/gs;
+$plainTextInOneLine =~ s//"/gs;
+$plainTextInOneLine =~ s//./gs;
+$plainTextInOneLine =~ s//-/gs;
+$plainTextInOneLine =~ s//-/gs;
+$plainTextInOneLine =~ s/\xc2\xa0/ /gs;
+
+# start from the 5% so skip the table of contents part.
 $startPos = length($plainTextInOneLine) * 0.05;
 
 $content = substr($plainTextInOneLine, $startPos);
 
-#subsitution.
-Item7($content);
-Item7a($content);
-Item89($content);
+# Pattern matching.
+Item7();
+Item7a();
+Item89();
 
-#print $content;
+open (FILETEST, "> contentdebug");
+print FILETEST $content;
+close FILETEST;
 
-#split lines into array.
+# print $content;
+
+# split lines into array.
 @all = split /\#\#\#\#\#\#/, $content;
-#prepare 2 empty strings for output.
+# prepare 2 empty strings for output.
 $output7="";
 $output7a="";
 $exist7=0;
 $exist7a=0;
 
-#now it's in such a pattern.
-# sth######ITEM7######Content of Item 7######ITEM7a######Content of Item7a
+# now it's in such a pattern.
+#  sth######ITEM7######Content of Item 7######ITEM7a######Content of Item7a
 for($i = 0; $i < scalar(@all); ++$i){
   if($all[$i] =~ m/^ITEM (7|7a|8|9)$/s){
-    #now $all[$i+1] should store the string I want.
+    # now $all[$i+1] should store the string I want.
     if($all[$i] =~ m/^ITEM 7$/s){
       $exist7=1;
-      #a do-until block is used here to add consecutive segments together.
-      #In some cases, there might be multiple ITEM7(Continue) in the file which will mess up the file. The loop will add them all together.
+      # a do-until block is used here to add consecutive segments together.
+      # In some cases, there might be multiple ITEM7(Continue) in the file which will mess up the file. The loop will add them all together.
       do{
         $output7.=$all[++$i];
       } until($all[$i+1] =~ m/^ITEM 7a$/s
@@ -126,16 +149,16 @@ for($i = 0; $i < scalar(@all); ++$i){
   }
 }
 
-#Output to 2 individual files.
+# Output to 3 individual files.
+$fileout=$filename."_out";
 $item7file=$filename."_7";
 $item7afile=$filename."_7a";
-$fileout=$filename."_out";
 open (FILEOUT, "> $fileout");
 print FILEOUT $plainTextInOneLine;
 close FILEOUT;
 if($exist7){
   open (ITEM7, "> $item7file");
-  print ITEM7 "ITEM 7. MANAGEMENT’S DISCUSSION AND ANALYSIS OF FINANCIAL CONDITION AND RESULTS OF OPERATIONS\n\n";
+  print ITEM7 "ITEM 7. MANAGEMENT'S DISCUSSION AND ANALYSIS OF FINANCIAL CONDITION AND RESULTS OF OPERATIONS\n\n";
   print ITEM7 $output7;
   close ITEM7;
   print "Item 7 output to file: $item7file.\n";
